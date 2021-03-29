329f0bf7ddab102efc366ef4c13fbb665ed10181
suhanacharya <suhan.acharya1@gmail.com>
2021-03-29 13:06:03 +0530

    Added dict.com and merriam-webster wod modules

--- helper.py
+++ helper.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-import datetime
 
 word_pattern = r'\S+'
 
@@ -18,6 +17,7 @@ def get_wod_oed_link(source):
 def get_wod_oed(source):
     link = get_wod_oed_link(source)
 
+    print("Getting oed word definition...")
     main_page = requests.get(link)
     soup = BeautifulSoup(main_page.content, 'html.parser')
 
@@ -26,14 +26,75 @@ def get_wod_oed(source):
 
     word = " ".join(re.findall(word_pattern, word[0].text))
     definition = " ".join(re.findall(word_pattern, definition[0].text))
-    current_date = datetime.datetime.now()
 
     d = {
         "source": "OED",
-        "date": str(current_date),
         "link": link, 
         "word": word,
         "definition": definition
     }
+
+    print("Gathered WOD from OED: success")
+
+    return d
+
+def get_wod_mw(source):
+    print('Getting wod from mw...')
+    main_page = requests.get(source)
+    soup = BeautifulSoup(main_page.content, 'html.parser')
+
+    word = soup.find_all('h1')[0].text
+
+    link = 'https://www.merriam-webster.com/dictionary/' + word
+    # print(word)
+    # print(link)
+
+    print("Getting mw definition of WOD...")
+
+    word_page = requests.get(link)
+    soup = BeautifulSoup(word_page.content, 'html.parser')
+
+    definition = soup.find_all('div', class_="vg")
+    word_pattern_sen = r'\S+'
+
+    # definition = re.findall(word_pattern_sen, definition[0].text)
+    definition = " ".join(re.findall(word_pattern_sen, definition[0].text))
+    definition = re.findall(r'\D+', definition)
+    definition = [x.strip() for x in definition]
+
+    d = {
+        "source": "MW",
+        "link": link, 
+        "word": word,
+        "definition": definition
+    }
+    
+    print("Gathered WOD from MW: success")
+
+    return d
+
+def get_wod_dict(source):
+    print('Getting wod from dictionary.com...')
+
+    main_page = requests.get(source)
+    soup = BeautifulSoup(main_page.content, 'html.parser')
+
+    word = soup.find_all('div', class_="otd-item-headword__word")[0].text
+    word = " ".join(re.findall(word_pattern, word))
+
+    print("Getting dictionary.com word definition...")
+    definition = soup.find_all('div', class_="otd-item-headword__pos")[0]
+    definition = definition.contents[-2].text
+
+    # print(definition)
+
+    d = {
+        "source": "Dictionary.com",
+        "link": source,
+        "word": word,
+        "definition": definition
+    }
+
+    print("Gathered WOD from dictionary.com: success")
 
     return d
